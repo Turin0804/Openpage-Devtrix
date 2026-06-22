ab69a2b761073992b42ada62b384fdfb0fd0ad77
mifta <mifta@gmail.com>
2026-06-22 23:37:30 +0600

    feat: recovery options

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -123,3 +123,53 @@ const Login = () => {
                                 </div>
                             </div>
                         </div>
+                       <div>
+                            <button
+                                type="submit"
+                                className="bg-indigo-500 w-full rounded-md py-3 text-white"
+                            >
+                                {loading ? (
+                                    <TbFidgetSpinner className="animate-spin m-auto" />
+                                ) : (
+                                    "Continue"
+                                )}
+                            </button>
+                        </div>
+                    </form>
+                    <div className="space-y-1">
+                        <button className="text-xs hover:underline hover:text-indigo-500 text-gray-400">
+                            Forgot password?
+                        </button>
+                    </div>
+                    <div className="flex items-center pt-4 space-x-1">
+                        <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                        <p className="px-3 text-sm dark:text-gray-400">
+                            Login with social accounts
+                        </p>
+                        <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                    </div>
+                    <div
+                        onClick={handleGoogleSignIn}
+                        className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+                    >
+                        <FcGoogle size={32} />
+
+                        <p>Continue with Google</p>
+                    </div>
+                    <p className="px-6 text-sm text-center text-gray-400">
+                        Don&apos;t have an account yet?{" "}
+                        <Link
+                            to="/signup"
+                            className="hover:underline hover:text-indigo-500 text-gray-600"
+                        >
+                            Sign up
+                        </Link>
+                        .
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
