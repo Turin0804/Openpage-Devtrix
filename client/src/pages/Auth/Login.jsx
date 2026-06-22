60dac6c791fd16417fb7f22c37a1917b74d668ad
mifta <mifta@gmail.com>
2026-06-22 23:27:47 +0600

    feat: add login form and Google sign-in

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -21,3 +21,105 @@ const Login = () => {
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
+    // form submit handler
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        try {
+            //User Login
+            await signIn(email, password);
+            navigate(from, { replace: true });
+            toast.success("Login Successful");
+        } catch (err) {
+            console.log(err);
+            toast.error(err?.message);
+        }
+    };
+
+    // Handle Google Signin
+    const handleGoogleSignIn = async () => {
+        try {
+            //User Registration using google
+            const data = await signInWithGoogle();
+            // Save user in database if not exists
+            await saveUser(data?.user);
+
+            navigate(from, { replace: true });
+            toast.success("Login Successful");
+        } catch (err) {
+            console.log(err);
+            toast.error(err?.message);
+        }
+    };
+    return (
+        <>
+            <Navbar />
+            <div className="flex justify-center items-center min-h-screen bg-white py-12">
+                <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+                    <div className="mb-8 text-center">
+                        <h1 className="my-3 text-4xl font-bold">Log In</h1>
+                        <p className="text-sm text-gray-400">
+                            Sign in to access your account
+                        </p>
+                    </div>
+                    <form
+                        onSubmit={handleSubmit}
+                        noValidate=""
+                        action=""
+                        className="space-y-6 ng-untouched ng-pristine ng-valid"
+                    >
+                        <div className="space-y-4">
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm"
+                                >
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    required
+                                    placeholder="Enter Your Email Here"
+                                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-indigo-500 bg-gray-200 text-gray-900"
+                                    data-temp-mail-org="0"
+                                />
+                            </div>
+                            <div>
+                                <div className="flex justify-between">
+                                    <label
+                                        htmlFor="password"
+                                        className="text-sm mb-2"
+                                    >
+                                        Password
+                                    </label>
+                                </div>
+                                <div className="relative">
+                                    <input
+                                        type={
+                                            showPassword ? "text" : "password"
+                                        }
+                                        name="password"
+                                        autoComplete="new-password"
+                                        id="password"
+                                        required
+                                        placeholder="*******"
+                                        className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-indigo-500 bg-gray-200 text-gray-900"
+                                    />
+                                    <div
+                                        className="absolute inset-y-5 right-0 pr-3 flex items-center cursor-pointer"
+                                        onClick={togglePasswordVisibility}
+                                    >
+                                        {showPassword ? (
+                                            <AiFillEyeInvisible size={24} />
+                                        ) : (
+                                            <AiFillEye size={24} />
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
