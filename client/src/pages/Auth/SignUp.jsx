44ea15cac4af16c47f3e489a4c748a504166728d
MiftahulJannatEfa <mifta@gmail.com>
2026-06-23 11:03:52 +0600

    feat: add imports for SignUp

--- client/src/pages/Auth/SignUp.jsx
+++ client/src/pages/Auth/SignUp.jsx
@@ -3,3 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import useAuth from "../../hooks/useAuth";
+import { toast } from "react-hot-toast";
+import { TbFidgetSpinner } from "react-icons/tb";
+import { saveUser, uploadImage } from "../../api/utils";
+import Navbar from "../../components/common/Navbar";
