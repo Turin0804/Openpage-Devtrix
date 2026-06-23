4fe8c67c6b1e1869855451275d37b4d76598f676
mifta <mifta@gmail.com>
2026-06-23 10:34:35 +0600

    feat:  initial imports for SignUp

---
+++ client/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import useAuth from "../../hooks/useAuth";
