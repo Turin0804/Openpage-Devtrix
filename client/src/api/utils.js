db9e631301b8304582ebff4a5a4d9dfaa3f03c4b
runa-74e <runa122001@gmail.com>
2026-06-23 12:31:25 +0600

    feat: add uploadImage utility function for image uploads

---
+++ client/src/api/utils.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// Upload image and return the URL
+export const uploadImage = async (imageData) => {
+    const formData = new FormData();
+    formData.append("image", imageData);
+
+    // send image to imgbb
+    const { data } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${
+            import.meta.env.VITE_IMGBB_API_KEY
+        }`,
+        formData
+    );
+    return data.data.display_url;
+};
