560257c417983f6479fa442f35134dfd7485555d
Kamrun Nesa <nesakamrun490@gmail.com>
2026-06-23 10:57:38 +0600

    feat: add Technology Innovation

---
+++ client/src/components/Home/TechnologyInnovation.jsx
@@ -0,0 +1,35 @@
+const TechnologyInnovation = () => {
+    const techTopics = [
+        {
+            id: 1,
+            title: "The Rise of AI in Everyday Life",
+            description: "Exploring how AI is transforming various industries.",
+            link: "https://medium.com/@dixitra20/the-rise-of-ai-in-everyday-life-how-artificial-intelligence-is-transforming-our-daily-experiences-1aa7bce6b9e0",
+        },
+        {
+            id: 2,
+            title: "Startups to Watch in 2025",
+            description: "A look at the most promising startups this year.",
+            link: "https://medium.com/@dixitra20/the-rise-of-ai-in-everyday-life-how-artificial-intelligence-is-transforming-our-daily-experiences-1aa7bce6b9e0",
+        },
+        {
+            id: 3,
+            title: "Understanding Large Language Models (LLMs)",
+            description: "An in-depth guide to LLMs and their applications.",
+            link: "https://medium.com/@dixitra20/the-rise-of-ai-in-everyday-life-how-artificial-intelligence-is-transforming-our-daily-experiences-1aa7bce6b9e0",
+        },
+        {
+            id: 4,
+            title: "The Future of IoT",
+            description: "How IoT is shaping the future of connected devices.",
+            link: "https://medium.com/@dixitra20/the-rise-of-ai-in-everyday-life-how-artificial-intelligence-is-transforming-our-daily-experiences-1aa7bce6b9e0",
+        },
+        {
+            id: 5,
+            title: "Cloud Computing Trends in 2025",
+            description: "Emerging trends in cloud computing and their impact.",
+            link: "https://medium.com/@dixitra20/the-rise-of-ai-in-everyday-life-how-artificial-intelligence-is-transforming-our-daily-experiences-1aa7bce6b9e0",
+        },
+    ];
+
+    
